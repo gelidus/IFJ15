07596da62a61ff42d244fd3e54906b3657ee4303
Josefsysel <pepa.ssl@seznam.cz>
2015-10-16 00:59:10 +0200

    Create errors.h

---
+++ errors.h
@@ -0,0 +1,21 @@
+enum compiler_error_type {
+  NO_ERROR,
+  ERR_LEX,
+  ERR_SYN,
+  ERR_SEM_UNDEF,
+  ERR_SEM_TYPE,
+  ERR_TYPE_INFERENCE,
+  ERR_SEM_OTHER,
+  ERR_RNT_NUM,
+  ERR_RNT_NIN,
+  ERR_RNT_ZER_DIV,
+  ERR_RNT_OTHER,
+  ERR_INT
+};
+
+struct compiler_error {
+  int return_value;
+  char* err_msg;
+};
+
+void throw_error(enum compiler_error_type, const char *);
