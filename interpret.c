b75588871a5a0391169fc5a6d5c3a5a0320ed36c
Gelidus <gelidus@flowup.eu>
2015-10-25 10:37:45 +0100

    [Interpret] Added definitions of interpret functions

--- interpret.c
+++ interpret.c
@@ -4,3 +4,39 @@
 void InterpretAST(struct ast_node* tree) {
 
 }
+
+void InterpretStatement(struct ast_node *statement) {
+
+}
+
+void InterpretVarDeclaration(struct ast_node *var) {
+
+}
+
+void InrerpretAssign(struct ast_node *statement) {
+
+}
+
+void InterpretReturn(struct ast_node *ret) {
+
+}
+
+void InterpretIf(struct ast_node *ifstatement) {
+
+}
+
+void InterpretFunctionDeclaration(struct ast_node *func) {
+
+}
+
+void InterpretFunctionDefinition(struct ast_node *func) {
+
+}
+
+void InterpretFunctionCall(struct ast_node *func) {
+
+}
+
+void InterpretBinaryOperation(struct ast_node *op) {
+
+}
